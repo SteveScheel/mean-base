eb936fcc86d58c2416c265817ea517bbafb913bd
Steve Scheel <nmu.sscheel@gmail.com>
2015-06-15 19:58:13 -0400

    Added a couple packages used for routing.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app     = express();
-var path    = require('path');
-var mongoose = require('mongoose');
-var port    = process.env.PORT || 8080; // use 8080 for localhost testing
+var express    = require('express');
+var app        = express();
+var path       = require('path');
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+var port       = process.env.PORT || 8080; // use 8080 for localhost testing
 
 // setup static files folder
 app.use(express.static(__dirname + '/public'));
